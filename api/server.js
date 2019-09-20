0c41f7ddf25d0b6eb952961b6aec3f19346dffea
teej387 <teej387@yahoo.com>
2019-09-19 19:27:23 -0500

    Set up routers in server.js

--- api/server.js
+++ api/server.js
@@ -4,13 +4,17 @@ require('dotenv').config()
 
 console.log(process.env.JWT_SECRET)
 
-const usersRouter = require('./users/users-router.js')
+const usersRouter = require('../users/users-router.js')
+const questionsRouter = require('../questions/questions-router.js')
+const responsesRouter = require('../responses/responses-router.js')
 
 const server = express()
 
 server.use(express.json())
 
 server.use('/api/users', usersRouter)
+server.use('/api/users/questions', questionsRouter)
+server.use('/api/users/responses', responsesRouter)
 
 server.get('/', (req, res) => {
   res.send('Welcome to Mentor Me!')
