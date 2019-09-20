4af7bb7bd9ce261c553023b49424ab03d6778461
teej387 <teej387@yahoo.com>
2019-09-20 18:57:04 -0500

    Created users-model.js and set up all (find) helper methods

---
+++ users/users-model.js
@@ -0,0 +1,94 @@
+db = require('../database/dbConfig.js')
+
+module.exports = {
+  findUsers,
+  findUser,
+  findQuestions,
+  findQuestion,
+  findResponses,
+  addUser,
+  addQuestion,
+  addResponse,
+  updateUser,
+  updateQuestion,
+  updateResponse,
+  removeUser,
+  removeQuestion,
+  removeResponse
+}
+
+function findUsers() {
+  return db('users')
+}
+
+function findUser(user_id) {
+  if (user_id) {
+    return db('users').where({id}).first()
+  }else {
+    res.status(404).json({ message: 'User ID not found' })
+  }
+}
+
+function findQuestions(user_id) {
+  if (user_id) {
+    return db('questions as q')
+    .join('users as u', 'u.id', 'q.user_id')
+    .where(user_id)
+  } else {
+    res.status(404).json({ message: 'User ID not found' })
+  }
+}
+
+function findQuestion(question_id) {
+  if (question_id) {
+    return db('questions').where({ question_id }).first()
+  } else {
+    res.status(404).json({ message: 'Question ID not found' })
+  }
+}
+
+function findResponses(question_id) {
+  if (question_id) {
+    return db('responses as r')
+      .join('questions as q', 'q.id', 'r.question_id')
+      .where(question_id)
+  } else {
+    res.status(404).json({ message: 'Question ID not found' })
+  }
+}
+
+function addUser(user) {
+
+}
+
+function addQuestion(question) {
+
+}
+
+function addResponse(response) {
+
+}
+
+function updateUser(changes, user_id) {
+  
+}
+
+function updateQuestion(changes, question_id) {
+
+}
+
+function updateResponse(changes, response_id) {
+
+}
+
+function removeUser(user_id) {
+  
+}
+
+function removeQuestion(question_id) {
+
+}
+
+function removeResponse(response_id) {
+
+}
