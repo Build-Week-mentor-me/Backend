addb601f319b810321f2e97cbb4bb8249d5d8393
teej387 <teej387@yahoo.com>
2019-09-21 19:01:01 -0500

    Changed requirements for the questions table.

---
+++ database/migrations/20190921184511_users-tables.js
@@ -0,0 +1,43 @@
+exports.up = function (knex) {
+  //  USERS TABLE
+  return knex.schema.createTable('users', tbl => {
+    tbl.increments()
+    tbl.string('username', 128).unique().notNullable()
+    tbl.string('password', 128).notNullable()
+    tbl.string('position', 128).notNullable()
+  })
+    // QUESTIONS TABLE
+    .createTable('questions', tbl => {
+      tbl.increments()
+      tbl.integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+      tbl.string('username', 128)
+      tbl.string('position', 128)
+      tbl.string('question', 500).notNullable()
+      tbl.string('business-type', 128)
+    })
+
+    //  RESPONSES TABLE
+    .createTable('responses', tbl => {
+      tbl.increments()
+      tbl.integer('question_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('questions')
+      tbl.string('username', 128).notNullable()
+      tbl.string('position', 128).notNullable()
+      tbl.string('response', 500).notNullable()
+      tbl.string('business-type', 128)
+    })
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('responses')
+    .dropTableIfExists('questions')
+    .dropTableIfExists('users')
+};
