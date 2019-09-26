a342be7cef0cb926100bdbc262c8b74d0c728c28
teej387 <teej387@yahoo.com>
2019-09-26 00:56:33 -0500

    Removed console.log

--- users/users-router.js
+++ users/users-router.js
@@ -42,7 +42,6 @@ router.post('/login', (req, res) => {
 
   Users.findBy({ username }).first()
     .then(user => {
-      console.log(user)
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user)
 
